96c227012fa5971168789de22e2f44436998cfbb
Ellis Green <devellisper@outlook.com>
2020-08-29 23:31:57 +0100

    Imported Alpinejs

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,5 +1,7 @@
 <x-layout.app>
     <h2 class="font-semibold">Welcome to <strong>Pace</strong>!</h2>
 
-    <p class="mt-2 text-gray-700">Get started building your website using this template</p>
+    <p class="mt-2 text-gray-700">
+        Get started building your website using this template
+    </p>
 </x-layout.app>
