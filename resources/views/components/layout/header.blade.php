414b21c335ed793925b924b2a3e7e8e16e17a7f3
Ellis Green <devellisper@outlook.com>
2021-05-08 16:48:59 +0100

    Updated composer dependencies

--- resources/views/components/layout/header.blade.php
+++ resources/views/components/layout/header.blade.php
@@ -1,7 +1,7 @@
 <header class="mb-6 bg-gradient-to-r from-purple-700 to-pink-700 text-white">
     <div class="container mx-auto flex justify-between items-center h-16">
         <h1 class="text-2xl font-semibold tracking-wide">
-            Pace
+            <a href="/">Pace</a>
         </h1>
 
         <nav class="flex space-x-4">
